44230a6e2ecde5d9aa5df62425ee806eec7224ce
yedhu <yedhukrishnaam@gmail.com>
2025-07-15 21:27:30 +0530

    added some of the js code with function

--- Temperature Converter/jscript.js
+++ Temperature Converter/jscript.js
@@ -2,3 +2,16 @@ const cal = document.getElementById("cel");
 const far = document.getElementById("far");
 const kel = document.getElementById("kel");
 
+function cal(event){
+    const currentValue = +event.target.value;
+
+    switch(event.target.value){
+        case "cal":
+            kel.value = (currentValue + 273.32).toFixed(2);
+            far.value = (currentValue * 1.8 +32).toFixed(2);
+            break;
+        case "far":
+            cal.value    
+    }
+        
+}
